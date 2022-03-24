8180f9532dfa4662d6fa25b895347156d0a8ce31
Fred <fred.vass@joi.events>
2022-03-24 17:34:25 +1100

    Added dash
    added a basic dash ability, removed sprint ability

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// Code Taken from https://sharpcoderblog.com/blog/unity-3d-fps-controller 
+// Basic Movement and Camera controls Taken from https://sharpcoderblog.com/blog/unity-3d-fps-controller 
 [RequireComponent(typeof(CharacterController))]
 
 public class PlayerMove : MonoBehaviour
 {
     public float walkingSpeed = 7.5f;
-    public float runningSpeed = 11.5f;
+    //public float runningSpeed = 11.5f;
+    public float dashSpeed = 50.0f;
+    public float dashTime = 0.2f;
     public float jumpSpeed = 8.0f;
     public float gravity = 20.0f;
     public Camera playerCamera;
@@ -17,6 +19,7 @@ public class PlayerMove : MonoBehaviour
 
     CharacterController characterController;
     Vector3 moveDirection = Vector3.zero;
+    Vector3 dashDirection = Vector3.zero;
     float rotationX = 0;
 
     [HideInInspector]
@@ -39,9 +42,9 @@ public class PlayerMove : MonoBehaviour
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
         // Press Left Shift to run
-        bool isRunning = Input.GetKey(KeyCode.LeftShift);
-        float curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical") : 0;
-        float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
+        //bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        float curSpeedX = canMove ? (/*isRunning ? runningSpeed : */walkingSpeed) * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? (/*isRunning ? runningSpeed : */walkingSpeed) * Input.GetAxis("Horizontal") : 0;
         float movementDirectionY = moveDirection.y;
         moveDirection = (forward * curSpeedX) + (right * curSpeedY);
 
@@ -52,6 +55,12 @@ public class PlayerMove : MonoBehaviour
         else
         {
             moveDirection.y = movementDirectionY;
+        }
+
+        //Dashing
+        if(Input.GetKeyDown("left shift"))
+        {
+            StartCoroutine(Dash());
         }
 
         // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
@@ -73,5 +82,27 @@ public class PlayerMove : MonoBehaviour
             playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
         }
+    }
+
+    IEnumerator Dash()
+    {
+        float startTime = Time.time;
+        
+        //Set the direction of the dash
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+        float dashSpeedX = canMove ? dashSpeed * Input.GetAxis("Vertical") : 0;
+        float dashSpeedY = canMove ? dashSpeed * Input.GetAxis("Horizontal") : 0;
+        dashDirection = (forward * dashSpeedX) + (right * dashSpeedY);
+
+        while(Time.time < startTime + dashTime)
+        {
+            canMove = false;
+            characterController.Move(dashDirection * Time.deltaTime);
+
+            yield return null;
+        }
+
+        canMove = true;
     }
 }
