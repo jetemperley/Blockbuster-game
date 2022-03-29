dcf8b91303b529ba0c87f914427dd9bdb891a1d0
jvisvikis <69331363+jvisvikis@users.noreply.github.com>
2022-03-29 16:22:48 +1100

    MinigunFire

---
+++ Assets/Scripts/MinigunFire.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinigunFire : MonoBehaviour
+{
+    public Camera cam;
+    public Bullet bulletPrefab;
+    public float fireCooldown; //seconds
+    public float fireRadius;
+
+    private AudioSource audioData;
+    private Animator animator;
+
+    public GameObject spawnPoint;
+
+    private float fireTimer; //seconds
+    // Start is called before the first frame update
+    void Start()
+    {
+        fireTimer = 0f;
+        //audioData = GetComponent<AudioSource>();
+        //audioData.Stop();
+        animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && fireTimer <= 0)
+        {
+            //audioData.Play(0);
+            animator.SetTrigger("Shoot");
+            Bullet bullet = Instantiate(bulletPrefab);
+            bullet.transform.parent = spawnPoint.transform;
+            bullet.transform.localPosition = Vector3.zero;
+            bullet.transform.localRotation = Quaternion.Euler(90, 0, 0);
+            bullet.transform.parent = null;
+            bullet.SetDir(cam.transform.forward + new Vector3(0.01f+Random.Range(fireRadius,-fireRadius),0.01f+Random.Range(fireRadius,-fireRadius),0));
+            fireTimer = fireCooldown;
+        }
+
+        fireTimer -= Time.deltaTime;
+    }
+}
