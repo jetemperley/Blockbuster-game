8be6515e8ec9e5153075d2b5738a63c555c50c8c
jetemperley <57704985+jetemperley@users.noreply.github.com>
2022-04-02 12:42:00 +1100

    added basic homing to cube

---
+++ Assets/Prefabs/NormalCube/CubeHomeBehaviour.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeHomeBehaviour : MonoBehaviour
+{
+    public Transform target;
+    Rigidbody rb;
+    public float moveSpeed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate() {
+        if (target == null)
+            return;
+        rb.MovePosition( rb.position +
+            (target.position - rb.position).normalized*moveSpeed*Time.fixedDeltaTime);
+    }
+}
--- Assets/Scripts/HealthParticles.cs
+++ Assets/Scripts/HealthParticles.cs
@@ -10,7 +10,6 @@ public class HealthParticles : Health
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("started special break");
     }
 
     // Update is called once per frame
