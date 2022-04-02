aef289581d4843e2ff7cb21162befdadd896a67c
Fred <fred.vass@joi.events>
2022-04-02 15:01:45 +1100

    created prefabs
    created prefabs for conductor and several test terrain pieces

--- Assets/Scripts/Terrain.cs
+++ Assets/Scripts/Terrain.cs
@@ -6,21 +6,23 @@ using UnityEngine;
 public class Terrain : MonoBehaviour
 {
     Rigidbody rigidbody;
+    Conductor conductor;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
         rigidbody.isKinematic = true;
+        conductor = Conductor.conductor;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Move the terrain towards the player at speed determined by the Conductor
-        transform.position += Vector3.back * Time.deltaTime * Conductor.conductor.getLevelSpeed();
+        transform.position += Vector3.back * Time.deltaTime * conductor.getLevelSpeed();
 
-        if (transform.position.z <= Conductor.conductor.getBoundary())
+        if (transform.position.z <= conductor.getBoundary())
         {
             transform.position = new Vector3(Random.Range(-20.0f, 20.0f), transform.position.y, 60.0f);
         }
