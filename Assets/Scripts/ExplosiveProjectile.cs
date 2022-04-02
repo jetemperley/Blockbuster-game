765b4cb2a5714f0375ddd8086f651092b7865307
jvisvikis <69331363+jvisvikis@users.noreply.github.com>
2022-04-02 12:41:18 +1100

    done did explosion

--- Assets/Scripts/ExplosiveProjectile.cs
+++ Assets/Scripts/ExplosiveProjectile.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ExplosiveProjectile : MonoBehaviour
 {
     public float projectileVelocity;
+    public float explosiveRadius;
     public Vector3 offset;
 
     private Rigidbody rb;
@@ -25,7 +26,26 @@ public class ExplosiveProjectile : MonoBehaviour
         
     }
 
-    void FixedUpdate(){
+    void ExplosionDamage(Vector3 center, float radius)
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(center, radius);
+        foreach(Collider hc in hitColliders){
+            if(hc.gameObject.name != "Plane"){
+               Destroy(hc.gameObject); 
+            }
+            
+        }
+        
+        Destroy(this.gameObject);
+       
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {        
+        if(collision.gameObject.name != "Player" && collision.gameObject != this.gameObject){
+            Debug.Log(collision.gameObject);
+            ExplosionDamage(transform.position, explosiveRadius);
+        }
         
     }
 
