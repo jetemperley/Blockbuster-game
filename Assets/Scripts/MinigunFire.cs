8f4253286e5c71dbbab60d2645f6e22e5abf0fb4
jvisvikis <69331363+jvisvikis@users.noreply.github.com>
2022-04-10 12:43:14 +1000

    minigun prefab, script and animation fixed for weapon pickup

--- Assets/Scripts/MinigunFire.cs
+++ Assets/Scripts/MinigunFire.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class MinigunFire : MonoBehaviour
 {
-    public Camera cam;
+    
     public Bullet bulletPrefab;
     public float fireCooldown; //seconds
     public float fireRadius;
 
+    private Camera cam;
     private AudioSource audioData;
     private Animator animator;
 
@@ -22,6 +23,7 @@ public class MinigunFire : MonoBehaviour
         //audioData = GetComponent<AudioSource>();
         //audioData.Stop();
         animator = GetComponent<Animator>();
+        cam = (Camera)FindObjectOfType(typeof(Camera));
 
     }
 
@@ -38,7 +40,7 @@ public class MinigunFire : MonoBehaviour
             bullet.transform.localPosition = Vector3.zero;
             bullet.transform.localRotation = Quaternion.Euler(90, 0, 0);
             bullet.transform.parent = null;
-            bullet.SetDir(spawnPoint.transform.forward + new Vector3(0.01f+Random.Range(fireRadius,-fireRadius),0.01f+Random.Range(fireRadius,-fireRadius),0));
+            bullet.SetDir(cam.transform.forward + new Vector3(Random.Range(fireRadius,-fireRadius),Random.Range(fireRadius,-fireRadius),0));
             fireTimer = fireCooldown;
         }else if(!Input.GetButton("Fire1"))
         {
