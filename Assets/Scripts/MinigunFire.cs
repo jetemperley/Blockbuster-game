e5d87cac2abf3dd7d58d4743d0f147c5103879b5
jetemperley <57704985+jetemperley@users.noreply.github.com>
2022-04-23 10:41:41 +1000

    hit scan implemented
    uses laser pool for efficiency
    lasers have custom duration
    -------------
    just use
        Laser laser = LaserPool.GetLaser();
        laser.fire(pos, dir, duration);

--- Assets/Scripts/MinigunFire.cs
+++ Assets/Scripts/MinigunFire.cs
@@ -16,7 +16,9 @@ public class MinigunFire : MonoBehaviour
     public GameObject spawnPoint;
 
     private float fireTimer; //seconds
-    // Start is called before the first frame update
+    LineRenderer line;
+
+
     void Start()
     {
         fireTimer = 0f;
@@ -24,6 +26,9 @@ public class MinigunFire : MonoBehaviour
         //audioData.Stop();
         animator = GetComponent<Animator>();
         cam = (Camera)FindObjectOfType(typeof(Camera));
+        line = gameObject.AddComponent<LineRenderer>();
+         line.startWidth = 0.1f;
+         line.endWidth = 0.1f;
 
     }
 
@@ -32,18 +37,29 @@ public class MinigunFire : MonoBehaviour
     {
         if (Input.GetButton("Fire1") && fireTimer <= 0)
         {
+
+            Vector3 dir = cam.transform.forward + new Vector3(Random.Range(fireRadius,-fireRadius),Random.Range(fireRadius,-fireRadius),0);
+
+            Laser laser = LaserPool.GetLaser();
+            laser.fire(
+                spawnPoint.transform.position,
+                dir,
+                0.1f
+                );
             //audioData.Play(0);
             animator.SetBool("Shooting",true);
-            Bullet bullet = Instantiate(bulletPrefab);
-            bullet.transform.parent = spawnPoint.transform;
-            bullet.transform.localPosition = Vector3.zero;
-            bullet.transform.localRotation = Quaternion.Euler(90, 0, 0);
-            bullet.transform.parent = null;
-            bullet.SetDir(cam.transform.forward + new Vector3(Random.Range(fireRadius,-fireRadius),Random.Range(fireRadius,-fireRadius),0));
+            // Bullet bullet = Instantiate(bulletPrefab);
+            // bullet.transform.parent = spawnPoint.transform;
+            // bullet.transform.localPosition = Vector3.zero;
+            // bullet.transform.localRotation = Quaternion.Euler(90, 0, 0);
+            // bullet.transform.parent = null;
+            // bullet.SetDir(dir);
             fireTimer = fireCooldown;
         }else if(!Input.GetButton("Fire1"))
         {
             animator.SetBool("Shooting",false);
+        } else {
+            line.enabled = false;
         }
 
         fireTimer -= Time.deltaTime;
--- Assets/Scripts/PistolFire.cs
+++ Assets/Scripts/PistolFire.cs
@@ -11,7 +11,6 @@ public class PistolFire : MonoBehaviour
     private Animator animator;
 
     public GameObject spawnPoint;
-    LineRenderer line;
 
     private float fireTimer; //seconds
     // Start is called before the first frame update
@@ -21,50 +20,37 @@ public class PistolFire : MonoBehaviour
         audioData = GetComponent<AudioSource>();
         audioData.Stop();
         animator = GetComponent<Animator>();
-         line = gameObject.AddComponent<LineRenderer>();
-         line.startWidth = 0.1f;
-         line.endWidth = 0.1f;
+        LaserPool.Init();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        line.enabled = false;
         if (Input.GetButtonDown("Fire1")&& fireTimer <=0)
         {
-            line.enabled = true;
+            
             audioData.Play(0);
             animator.SetTrigger("Shoot");
-            RaycastHit hit;
-
-            if (Physics.Raycast (spawnPoint.transform.position, spawnPoint.transform.forward, out hit, 1000)){
-                try{
-                    Health health = hit.collider.attachedRigidbody.gameObject.GetComponent<Health>();
-                    if (health != null){
-                    health.takeDamage(1);
-                    }
-                } catch{
-
-                }
-                Vector3[] arr = {spawnPoint.transform.position, hit.point};
-               line.SetPositions(arr);
-            }else{
-                Vector3[] arr = {spawnPoint.transform.position,spawnPoint.transform.forward*1000};
-                line.SetPositions(arr);
-                // Debug.DrawLine(spawnPoint.transform.position, spawnPoint.transform.position*1000);
-            }
-            Bullet bullet = Instantiate(bulletPrefab);
-            bullet.transform.parent = spawnPoint.transform;
-            bullet.transform.localPosition = new Vector3(0,0,0);
-            bullet.transform.localRotation = Quaternion.Euler(90,0,0);
-            bullet.transform.parent = null;
-            bullet.SetDir(spawnPoint.transform.forward);
+            
+            Laser laser = LaserPool.GetLaser();
+            laser.fire(
+                spawnPoint.transform.position,
+                spawnPoint.transform.forward*1000,
+                0.2f
+                );
+            
+            // Bullet bullet = Instantiate(bulletPrefab);
+            // bullet.transform.parent = spawnPoint.transform;
+            // bullet.transform.localPosition = new Vector3(0,0,0);
+            // bullet.transform.localRotation = Quaternion.Euler(90,0,0);
+            // bullet.transform.parent = null;
+            // bullet.SetDir(spawnPoint.transform.forward);
             fireTimer = fireCooldown;
         } else if (fireTimer > 0){
-             line.enabled = true;
+            fireTimer -= Time.deltaTime;
         }
 
-        fireTimer -= Time.deltaTime;
+        
     }
 }
