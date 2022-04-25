84497623dbf0d15d52de563a072ba97cd20278f0
jetemperley <57704985+jetemperley@users.noreply.github.com>
2022-04-25 15:51:53 +1000

    Revert "Added level loader example for big levels"
    This reverts commit 152030eee74cb137491a8cd8e788a25494b6c43b.

--- Assets/Scripts/Enabler.cs
+++ Assets/Scripts/Enabler.cs
@@ -1,55 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Threading;
 
 public class Enabler : MonoBehaviour
 {
     
     public GameObject enableThis;
-    float toOffset = 0;
-    int layers = 2;
+
     void Start()
     {
-        //disable(layers, enableThis.transform);
         enableThis.SetActive(false);
     }
 
     void FixedUpdate() {
-        // toOffset = Time.fixedDeltaTime * Conductor.conductor.getLevelSpeed();
+        enableThis.transform.position = transform.position + Vector3.back * Time.fixedDeltaTime * Conductor.conductor.getLevelSpeed();
     }
 
     void OnTriggerEnter(Collider other) {
-        if (other.attachedRigidbody == null)
-            return; 
-        if (other.attachedRigidbody.gameObject.CompareTag("Player")) {
-            // StartCoroutine(load(layers, enableThis.transform));
+        
+        // for (int i = 0; i < transform.childCount; i++){
+        //     GameObject child = transform.GetChild(i).gameObject;
+        //     child.SetActive(true);
+        // }
+        if (other.attachedRigidbody.gameObject.CompareTag("Player"))
             enableThis.SetActive(true);
-        }
-            
 
-    }
-    
+        gameObject.SetActive(false);
 
-    IEnumerator load(int depth, Transform t){
 
-        for (int i = 0; i < t.transform.childCount; i++){
-            if (depth > 1)
-                yield return load(depth -1, t.transform.GetChild(i));
-            GameObject child = t.transform.GetChild(i).gameObject;
-            child.SetActive(true);
-            yield return null;
-        }
-
-    }
-
-    void disable(int depth, Transform t){
-        for (int i = 0; i < t.transform.childCount; i++){
-            if (depth > 1)
-                load(depth -1, t.transform.GetChild(i));
-            GameObject child = t.transform.GetChild(i).gameObject;
-            child.SetActive(false);
-        }
     }
 
 
