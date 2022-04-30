10638dac824787834d211d8cbf8f77aa1a168cf3
jvisvikis <69331363+jvisvikis@users.noreply.github.com>
2022-04-30 11:29:14 +1000

    stuff

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    private GameManager gameManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //gameManager =
     }
 
     // Update is called once per frame
