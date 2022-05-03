f365e20c9271aec1f7ac9bbe0f4a2f2fac92608f
Fred <fred.vass@joi.events>
2022-05-03 18:55:50 +1000

    Updated boundary
    player boundary now has a transparent grid wall that appears when the player gets to close/passes it.

---
+++ Assets/Scripts/BoundaryGrid.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundaryGrid : MonoBehaviour
+{
+    GameObject player;
+    public MeshRenderer renderer;
+    Color materialColor;
+    public float distanceToAppear;
+    public float distanceToFade;
+    private float originalAlpha;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        materialColor = renderer.material.color;
+        originalAlpha = materialColor.a;
+        materialColor.a = 0;
+        player = GameObject.FindWithTag("Player");
+        renderer.material.color = materialColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null) 
+        {
+            if (player.transform.position.z < distanceToFade)
+                materialColor.a = 0;
+
+            if (player.transform.position.z >= distanceToFade && player.transform.position.z < distanceToAppear)
+                materialColor.a = ((player.transform.position.z - distanceToFade)/(distanceToAppear - distanceToFade))/originalAlpha;
+
+            if (player.transform.position.z >= distanceToAppear)
+                materialColor.a = originalAlpha;
+
+            renderer.material.color = materialColor;
+        }
+    }
+}
