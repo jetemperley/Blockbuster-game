a11c88cfd91f4fa77ff521f2ae978b71d01ceeeb
Fred <fred.vass@joi.events>
2022-05-03 16:01:07 +1000

    tryna fix merge conflicts

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -23,9 +23,9 @@ public class PlayerMove : MonoBehaviour
     Vector3 dashDirection = Vector3.zero;
     float rotationX = 0;
 
-    public float dashCooldown = 1.0f;
-    public bool canDash;
-    private float dashTimer;
+    //public float dashCooldown = 1.0f;
+    //public bool canDash;
+    //private float dashTimer;
 
     [HideInInspector]
     public bool canMove = true;
@@ -40,8 +40,8 @@ public class PlayerMove : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
 
-        dashTimer = 0;
-        canDash = true;
+        /*dashTimer = 0;
+        canDash = true;*/
     }
 
     // Update is called once per frame
@@ -67,14 +67,12 @@ public class PlayerMove : MonoBehaviour
         }
 
         //Dashing
-        if(Input.GetKeyDown("left shift") && canDash)
+        if(Input.GetKeyDown("left shift"))
         { 
-            canDash = false;
-            dashTimer = dashCooldown;
             StartCoroutine(Dash());
         }
 
-        if (dashTimer >= 0 && !canDash)
+        /*if (dashTimer >= 0 && !canDash)
         {
             dashTimer -= Time.deltaTime;
         }
@@ -82,7 +80,7 @@ public class PlayerMove : MonoBehaviour
         if (dashTimer <= 0)
         {
             canDash = true;
-        }
+        }*/
 
         // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
         // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
