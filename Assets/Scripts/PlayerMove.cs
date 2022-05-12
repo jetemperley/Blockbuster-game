1c0f964e0d312b80400162b2819f9adcbd2fe231
jetemperley <57704985+jetemperley@users.noreply.github.com>
2022-05-12 21:29:56 +1000

    BUG FIXES
    + player can look while dashing
    + player always falls at correct speed

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -77,7 +77,9 @@ public class PlayerMove : MonoBehaviour
             PlayerStats.getInst().addStat("jump");   
             moveDirection.y = jumpSpeed;
         }
-        else
+        else if (characterController.isGrounded){
+            moveDirection.y = 0;
+        } else 
         {
             moveDirection.y = movementDirectionY;
         }
@@ -116,13 +118,13 @@ public class PlayerMove : MonoBehaviour
         characterController.Move(moveDirection * Time.deltaTime);
 
         // Player and Camera rotation
-        if (canMove)
-        {
-            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
-            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
-            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
-            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
-        }
+        
+        
+        
+        rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+        rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+        playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
         canJump = false;
     }
 
@@ -138,9 +140,10 @@ public class PlayerMove : MonoBehaviour
         float dashSpeedY = canMove ? dashSpeed * Input.GetAxis("Horizontal") : 0;
         dashDirection = (forward * dashSpeedX) + (right * dashSpeedY);
 
+        canMove = false;
         while(Time.time < startTime + dashTime)
         {
-            canMove = false;
+            
             characterController.Move(dashDirection * Time.deltaTime);
 
             yield return null;
