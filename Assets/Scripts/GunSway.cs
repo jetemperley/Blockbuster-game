c20bf452b53ac13b010974722671e35a584124b3
CBickey <charlie.bickerton@students.mq.edu.au>
2022-05-23 14:54:34 +1000

    useless material adding
    only affects charlietest

--- Assets/Scripts/GunSway.cs
+++ Assets/Scripts/GunSway.cs
@@ -12,35 +12,37 @@ public class GunSway : MonoBehaviour
 
     // Update is called once per frame
     void Start(){
-        initialPos = transform.localPosition;
+        //initialPos = transform.localPosition;
     }
 
     void Update()
     {
-        // RotateSway();
-        MoveSway();
+        RotateSway();
+        //MoveSway();
     }
 
     private void RotateSway(){
         float mouseX = Input.GetAxisRaw("Mouse X")*swayMultiplier;
         float mouseY = Input.GetAxisRaw("Mouse Y")*swayMultiplier;
+        
 
         Quaternion rotationX = Quaternion.AngleAxis(-mouseY, Vector3.right);
         Quaternion rotationY = Quaternion.AngleAxis(mouseX, Vector3.up);
 
         Quaternion targetRotation = rotationX * rotationY;
+        Debug.Log(targetRotation);
 
         transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, smooth*Time.deltaTime);
+    
     }
 
     private void MoveSway(){
         float movementX = -Input.GetAxis("Mouse X")*swayMultiplier;
         float movementY = -Input.GetAxis("Mouse Y")*swayMultiplier;
-        // movementX = Mathf.Clamp(movementX, -maxAmount, maxAmount);
-        // movementY = Mathf.Clamp(movementY, -maxAmount, maxAmount);
+        movementX = Mathf.Clamp(movementX, -maxAmount, maxAmount);
+        movementY = Mathf.Clamp(movementY, -maxAmount, maxAmount);
 
         Vector3 finalPosition = new Vector3(movementX, movementY, 0);
         transform.localPosition = Vector3.Lerp(transform.localPosition, finalPosition+initialPos, Time.deltaTime*smooth);
-
     }
 }
