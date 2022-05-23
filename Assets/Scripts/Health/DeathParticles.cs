846ddfb4aadedc366a43078032238bf25aa2b9de
jetemperley <57704985+jetemperley@users.noreply.github.com>
2022-05-23 15:54:26 +1000

    fixed particles moving with terrain

--- Assets/Scripts/Health/DeathParticles.cs
+++ Assets/Scripts/Health/DeathParticles.cs
@@ -20,6 +20,7 @@ public class DeathParticles : DeathEffect
     // Start is called before the first frame update
     public override void effect(){
         particlesOnDeath.transform.SetParent(null);
+        particlesOnDeath.transform.position = transform.position;
         if(explodeScale != null){
             particlesOnDeath.gameObject.transform.localScale = new Vector3 (explosion,explosion,explosion);
         }
