4ece77219e12dd0db24331089163c7cfe61dc386
jetemperley <57704985+jetemperley@users.noreply.github.com>
2022-06-12 00:01:21 +1000

    end kills player

--- Assets/Scripts/endTrigger.cs
+++ Assets/Scripts/endTrigger.cs
@@ -9,6 +9,7 @@ public class endTrigger : MonoBehaviour {
     void OnTriggerEnter (Collider c){
         if (c.gameObject.CompareTag("Player")){
             GameObject.FindObjectOfType<GameManager>().CompleteLevel();
+            c.attachedRigidbody.gameObject.GetComponent<Health>().takeDamage(100);
         }
         
     }
