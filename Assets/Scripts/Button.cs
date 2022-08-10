1ef4964bde1fbde58be0989e4777c65795c72a24
jvisvikis <69331363+jvisvikis@users.noreply.github.com>
2022-08-10 17:07:51 +1000

    Added hover effect to UI buttons in main menu

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class Button : MonoBehaviour
-{
-     void OnMouseOver()
+
+public class Button : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+ {
+     private Vector4 origColour;
+     public Vector4 hovColour;
+     public Text text;
+ 
+     void Start()
     {
-        //If your mouse hovers over the GameObject with the script attached, output this message
-        Debug.Log("Mouse is over GameObject.");
+        origColour = GetComponent<Outline>().effectColor;
     }
 
-    void OnMouseExit()
-    {
-        //The mouse is no longer hovering over the GameObject so output this message each frame
-        Debug.Log("Mouse is no longer on GameObject.");
-    }
+     public void OnPointerEnter(PointerEventData eventData)
+     {
+        GetComponent<Outline>().effectColor = hovColour;
+        text.GetComponent<Outline>().effectColor = hovColour;
+     }
+ 
+     public void OnPointerExit(PointerEventData eventData)
+     {
+        GetComponent<Outline>().effectColor = origColour;
+        text.GetComponent<Outline>().effectColor = origColour;;
+     }
 }
