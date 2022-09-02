c495397effdabc73417bf1c6b4de58e5114ab281
jvisvikis <69331363+jvisvikis@users.noreply.github.com>
2022-09-03 00:23:37 +1000

    burstfire for tank is implemented

--- Assets/Scripts/TankMelee.cs
+++ Assets/Scripts/TankMelee.cs
@@ -9,6 +9,7 @@ public class TankMelee : MonoBehaviour
     public GameObject ChargingCircleUI;
     public GameObject AoEIndicatorCanvas;
 
+    //melee 
     public float maxMeleeRadius;
     public float startMeleeDistance;
     public int meleeDamage;
@@ -19,6 +20,18 @@ public class TankMelee : MonoBehaviour
     private bool isAttacking;
     private bool growChargeCircle;
     private float chargeCircleTimer;
+
+    //projectile
+    public float fireRate = 1;
+    public GameObject projectile;
+    public float projSpawnDist = 1;
+    public float projSpeed = 5;
+    public int projPerBurst;
+    public float projInterval;
+    public float burstFireCooldown;
+
+    private FieldOfView fov;
+    private float burstFireTimer;
     
 
     // Start is called before the first frame update
@@ -26,6 +39,7 @@ public class TankMelee : MonoBehaviour
     {
         player = FindObjectOfType<PlayerMove>();
         AoEIndicatorCanvas.SetActive(false);
+        fov = GetComponent<FieldOfView>();
     }
 
     // Update is called once per frame
@@ -37,6 +51,10 @@ public class TankMelee : MonoBehaviour
             StartCoroutine(AttackSequence());
             isAttacking = true;
 
+        }else if(dist > startMeleeDistance && fov.visibleTargets.Count > 0 && burstFireTimer <=0 && !isAttacking)
+        {
+            StartCoroutine(BurstFire(FindObjectOfType<PlayerMove>().gameObject));
+            burstFireTimer = burstFireCooldown;
         }
 
         if(growChargeCircle && Mathf.Abs(chargeCircleTimer/meleeChargeTime)<1)
@@ -48,6 +66,7 @@ public class TankMelee : MonoBehaviour
         }
 
         attackTimer -= Time.deltaTime;
+        burstFireTimer -= Time.deltaTime;
             
     }
 
@@ -59,6 +78,28 @@ public class TankMelee : MonoBehaviour
         meleeCharge.Stop();
         AttackPlayer();
         ResetCooldown();
+    }
+
+    private IEnumerator BurstFire(GameObject target)
+    {
+        for(int i = 0; i<projPerBurst; i++)
+        {
+            yield return new WaitForSeconds(projInterval*(i+1));
+            Shoot(target);
+        }
+        
+    }
+
+    private void Shoot(GameObject target)
+    {
+        GameObject g = Instantiate(projectile);
+        g.transform.SetParent(transform);
+        Vector3 dir = (target.transform.position - transform.position).normalized * projSpawnDist;
+
+        g.transform.position = transform.position + dir;
+        g.transform.SetParent(null);
+        Rigidbody prb = g.GetComponent<Rigidbody>();
+        prb.AddForce(dir.normalized*projSpeed, ForceMode.VelocityChange);
     }
     
     private void AttackPlayer()
