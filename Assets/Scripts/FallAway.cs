36053553b765c74e6b9e68433145ae814d7f0311
jvisvikis <69331363+jvisvikis@users.noreply.github.com>
2022-09-07 22:09:39 +1000

    Updated fall away force and fov for enemies

--- Assets/Scripts/FallAway.cs
+++ Assets/Scripts/FallAway.cs
@@ -42,8 +42,8 @@ public class FallAway : MonoBehaviour
             
             // rb.AddForce(randX, randY, randZ, ForceMode.VelocityChange);
             rb.AddForce(
-                Random.Range(0f, 1f), 
-                1, // Random.Range(0f, 1f), 
+                Random.Range(-100f, 100f), 
+                Random.Range(1f, 20f), // Random.Range(0f, 1f), 
                 0, 
                 ForceMode.VelocityChange);
             // rb.MovePosition(rb.position + fallSpeed*Time.deltaTime);
