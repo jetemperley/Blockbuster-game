4398a502e9ae3ac38e345d22f2b9f49aba162dee
CBickey <charlie.bickerton@students.mq.edu.au>
2022-10-09 16:07:39 +1100

    New Railgun Effects
    With delayed shot

--- Assets/Scripts/RailgunFire.cs
+++ Assets/Scripts/RailgunFire.cs
@@ -12,6 +12,8 @@ public class RailgunFire : MonoBehaviour
 
     private AudioSource audioData;
     private Animator animator;
+    public LineRenderer lineRenderer;
+    public ParticleSystem particle;
 
     public GameObject spawnPoint;
 
@@ -20,6 +22,15 @@ public class RailgunFire : MonoBehaviour
     public bool autoFire;
 
     private float fireTimer; //seconds
+        
+    private bool shootDelay;
+    private float shootDelayTimer;
+    public float shootDelayTime;
+
+    private float laserLineVisible;
+    public float laserLineVisibleTimer;
+    private Vector3[] linePoints;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,19 +40,51 @@ public class RailgunFire : MonoBehaviour
         audioData.Stop();
         //animator = GetComponent<Animator>();
         LaserPool.Init();
-        
+        linePoints = new Vector3[2];
+        lineRenderer.enabled=false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (((!autoFire && controls.actions["Fire"].triggered) | (autoFire && controls.actions["Fire"].ReadValue<float>() == 1)) && fireTimer <=0 && !PauseMenu.gameIsPaused)
-        {
-            
+
+
+    void Update(){
+
+
+        if (((!autoFire && controls.actions["Fire"].triggered) | (autoFire && controls.actions["Fire"].ReadValue<float>() == 1)) && fireTimer <=0 && !PauseMenu.gameIsPaused && shootDelay != true){
             audioData.Play(0);
+            particle.Play();
+            shootDelay=true;
+        }
+        if(shootDelay == true){ //start timer
+            shootDelayTimer+=Time.deltaTime;
+        }
+        if(shootDelayTimer>=shootDelayTime){ //shoot after timer 
+            Debug.Log("Line renderer is on!");
+            lineRenderer.enabled = true;
+            Shoot();
+            shootDelay=false;
+            shootDelayTimer=0;
+        }
+        if(fireTimer>=0){ //countdown fire cooldown
+            fireTimer -= Time.deltaTime;
+        }
+        if(lineRenderer.enabled == true){
+            laserLineVisible+= Time.deltaTime;
+            if(laserLineVisible>laserLineVisibleTimer){
+                Debug.Log("Line renderer is off!");
+                lineRenderer.enabled=false;
+                laserLineVisible=0;
+            }
+        }
+        //Debug.Log(laserLineVisible);
+        linePoints[0] = spawnPoint.transform.position; 
+        linePoints[1] = spawnPoint.transform.position+(spawnPoint.transform.forward*300.0f);
+        lineRenderer.SetPositions(linePoints);
+
+    }
+
+    public void Shoot(){
             //animator.SetTrigger("Shoot");
             Health health;
-
             foreach(RaycastHit hit in Physics.SphereCastAll(spawnPoint.transform.position, beamWidth, spawnPoint.transform.forward*1000, 300.0f))
             {
                 try{
@@ -56,10 +99,8 @@ public class RailgunFire : MonoBehaviour
                 } catch{}
             }        
             fireTimer = fireCooldown;
-        }else{
-        fireTimer -= Time.deltaTime;
         }
-    }
+    
 
     public void Fire(InputAction.CallbackContext ctx)
     {
