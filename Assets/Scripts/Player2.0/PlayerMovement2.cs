1d4c9c507a419e194751e3a3dfe496f2a701ecc9
CBickey <53806620+CBickey@users.noreply.github.com>
2022-10-23 19:11:49 +1100

    Update PlayerMovement2.cs

--- Assets/Scripts/Player2.0/PlayerMovement2.cs
+++ Assets/Scripts/Player2.0/PlayerMovement2.cs
@@ -102,7 +102,7 @@ public class PlayerMovement2 : MonoBehaviour
     private void Update()
     {
         // ground check
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.1f, whatIsGround);
 
         if(!PauseMenu.gameIsPaused)
         {
